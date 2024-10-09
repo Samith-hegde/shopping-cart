756c2cfe3eb350a47c60d696e62f564fcd8ec305
Samith-hegde <hegdesamith02@gmail.com>
2024-10-09 23:05:00 +0530

    Set up project skeleton

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# shopping-cart
+# Angadi: A Shopping Cart Project
+
+This is a Shopping Cart project developed as a SPA (Single Page Application) using ReactJS, as part of the React Module in The Odin Project Full Stack Development online course.
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.jsx',
+    output: {
+        filename: 'index.jsx',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Angadi',
+            template: './index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
