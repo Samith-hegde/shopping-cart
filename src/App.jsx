8dff2471851d708901b0be64e0cf1204ae9364ec
Samith-hegde <hegdesamith02@gmail.com>
2024-10-19 14:24:17 +0530

    Make styling consistent

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 import useInventory from './hooks/useInventory'
+import './App.css'
 
 function App() {
   const { inventory, changeQuantity } = useInventory();
